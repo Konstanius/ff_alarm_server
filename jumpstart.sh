7b5ae5c2b72667d01629af27cc6fdb7700edd9ff
Konstanius <earlygamerhd@gmail.com>
2024-03-24 17:27:56 +0100

    Expanded jumpstart.sh requirements

--- jumpstart.sh
+++ jumpstart.sh
@@ -3,27 +3,20 @@ set -e
 # Purpose of this script is to install dart 3.3.1, docker, and then run ./init/installer.dart
 # This script is intended to be run on a fresh Ubuntu 20.04 LTS installation
 
+# basic requirements: sudo, wget, curl, gpg, docker
+sudo apt-get update && sudo apt-get install -y sudo wget curl gpg docker apt-transport-https
+
 # Check if dart is not already installed
 if ! command -v dart &> /dev/null
 then
   echo "Installing Dart"
-  sudo apt-get update && sudo apt-get install apt-transport-https
   wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg
   echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-  sudo apt-get update && sudo apt-get install dart
+  sudo apt-get update && sudo apt-get install dart -y
 
   # Add Dart to PATH
   # shellcheck disable=SC2016
   echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.bashrc
-fi
-
-# Check if docker is not already installed
-if ! command -v docker &> /dev/null
-then
-  echo "Installing Docker"
-  sudo apt-get update
-  sudo apt-get install docker
-  sudo usermod -aG docker "$USER"
 fi
 
 # Run installer.dart
