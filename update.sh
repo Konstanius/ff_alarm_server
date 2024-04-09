30ef15718e678a40a5ba3e3d8e08a917cec7c885
Konstanius <earlygamerhd@gmail.com>
2024-04-09 14:09:18 +0200

    Updated buildDeploy.sh into update.sh which incorporates git pull command and stops the previous docker container

---
+++ update.sh
@@ -0,0 +1,30 @@
+set -e
+
+echo "Updating source"
+git pull
+
+echo "Stopping old docker container"
+docker stop ff_alarm_server || true
+
+echo "Getting dependencies of dart project"
+dart pub get
+
+echo "Compiling executable"
+dart compile exe main.dart -o resources/main.exe
+
+echo "Building docker image \"ff_alarm_server\""
+docker build -t ff_alarm_server .
+
+echo "Deleting old docker image \"ff_alarm_server\""
+docker container rm ff_alarm_server || true
+
+echo "Creating and starting new docker container \"ff_alarm_server\""
+docker create --hostname ff_alarm_server --name ff_alarm_server --network ff_alarm_network -v "$(pwd)/resources:/ff/resources" ff_alarm_server
+
+docker cp resources/main.exe ff_alarm_server:/ff/resources/main.exe
+docker start ff_alarm_server
+
+echo "Setting permissions for resources folder"
+sudo chmod -R 777 resources/
+
+echo "Successfully updated and started ff_alarm_server"
